7daecefbe7c36fb2404c83481b15240ff3171e84
Lazaros Topalis <83336644+ltopalis@users.noreply.github.com>
2023-01-09 16:23:25 +0200

    example adding processes using FCFS

--- PROJECT 2/scheduler/scheduler.c
+++ PROJECT 2/scheduler/scheduler.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "structures.h"
 #include "FCFS.h"
@@ -17,29 +18,65 @@
 int main(int argc, char **argv)
 {
 	/* local variables */
-	process_list root;
-	process_list *node;
+	process_list *root = (process_list *) malloc(sizeof(process_list));
+	process_list *node = (process_list *) malloc(sizeof(process_list));
 
-	process_info newInfo;
-	newInfo.elapsed_time = 111.02;
-	newInfo.history = NULL;
-	strcpy(newInfo.name, "k");
-	newInfo.PID = 1520;
-	newInfo.priority = 1;
-	newInfo.workload_time = 752.5;
-	
-	process_list newP;
-	newP.info = newInfo;
+	root->prev = NULL;
+	process_info data;
+	data.elapsed_time=1.01;
+	strcpy(data.name, "work1");
+	data.PID = 3627;
+	data.priority = 1;
+	data.workload_time = 1.01;
+	FCFSadd(data, root);
 
-	add(&newP, root);
+	data.elapsed_time=2.010;
+	strcpy(data.name, "work2");
+	data.PID = 3628;
+	data.priority = 1;
+	data.workload_time = 3.02;
+	FCFSadd(data, root);
 
-	node = &root;
-	printf("%p %p", &root, root.next);
+	data.elapsed_time=3.010;
+	strcpy(data.name, "work3");
+	data.PID = 3639;
+	data.workload_time = 6.03;
+	data.priority = 1;
+	FCFSadd(data, root);
+
+	data.elapsed_time=4.010;
+	strcpy(data.name, "work4");
+	data.PID = 3630;
+	data.workload_time = 10.04;
+	data.priority = 1;
+	FCFSadd(data, root);
+
+	data.elapsed_time=5.010;
+	strcpy(data.name, "work5");
+	data.PID = 3631;
+	data.workload_time = 15.05;
+	data.priority = 1;
+	FCFSadd(data, root);
+
+	data.elapsed_time=6.01;
+	strcpy(data.name, "work6");
+	data.PID = 3632;
+	data.workload_time = 21.06;
+	data.priority = 1;
+	FCFSadd(data, root);
+
+	data.elapsed_time=7.01;
+	strcpy(data.name, "work7");
+	data.PID = 3633;
+	data.workload_time = 28.07;
+	data.priority = 1;
+	FCFSadd(data, root);
+
+	node = root->next;
 	while(node != NULL){
-		printf("%s\n", node->info.name);
-        node = node->next;
-    }
-
+		toString(node->info);
+		node = node->next;
+	}
 
 	/* parse input arguments (policy, quantum (if required), input filename */
 
