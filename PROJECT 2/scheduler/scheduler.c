5eead3fe16b402519b53630bd616882089a399f1
Lazaros Topalis <83336644+ltopalis@users.noreply.github.com>
2023-01-09 19:22:31 +0200

    testing add-functions

--- PROJECT 2/scheduler/scheduler.c
+++ PROJECT 2/scheduler/scheduler.c
@@ -3,6 +3,9 @@
 #include <string.h>
 #include "structures.h"
 #include "FCFS.h"
+#include "SJF.h"
+#include "RR.h"
+#include "PRIO.h"
 /* header files */
 
 /* global definitions */
@@ -21,62 +24,68 @@ int main(int argc, char **argv)
 	process_list *root = (process_list *)malloc(sizeof(process_list));
 	process_list *node = (process_list *)malloc(sizeof(process_list));
 
+	root->next = NULL;
 	root->prev = NULL;
 	process_info data;
+
 	data.elapsed_time = 1.01;
 	strcpy(data.name, "work1");
 	data.PID = 3627;
-	data.priority = 1;
+	data.priority = 6;
 	data.workload_time = 1.01;
-	FCFSadd(data, root);
+	RRadd(data, root);
 
 	data.elapsed_time = 2.010;
 	strcpy(data.name, "work2");
 	data.PID = 3628;
-	data.priority = 1;
+	data.priority = 4;
 	data.workload_time = 3.02;
-	FCFSadd(data, root);
+	RRadd(data, root);
 
 	data.elapsed_time = 3.010;
 	strcpy(data.name, "work3");
 	data.PID = 3639;
 	data.workload_time = 6.03;
 	data.priority = 1;
-	FCFSadd(data, root);
+	RRadd(data, root);
 
 	data.elapsed_time = 4.010;
 	strcpy(data.name, "work4");
 	data.PID = 3630;
 	data.workload_time = 10.04;
-	data.priority = 1;
-	FCFSadd(data, root);
+	data.priority = 7;
+	RRadd(data, root);
 
 	data.elapsed_time = 5.010;
 	strcpy(data.name, "work5");
 	data.PID = 3631;
 	data.workload_time = 15.05;
-	data.priority = 1;
-	FCFSadd(data, root);
+	data.priority = 3;
+	RRadd(data, root);
 
 	data.elapsed_time = 6.01;
 	strcpy(data.name, "work6");
 	data.PID = 3632;
 	data.workload_time = 21.06;
-	data.priority = 1;
-	FCFSadd(data, root);
+	data.priority = 2;
+	RRadd(data, root);
 
 	data.elapsed_time = 7.01;
 	strcpy(data.name, "work7");
 	data.PID = 3633;
 	data.workload_time = 28.07;
-	data.priority = 1;
-	FCFSadd(data, root);
+	data.priority = 5;
+	RRadd(data, root);
 
 	node = root->next;
+	while (node->next != NULL)
+	{
+		node = node->next;
+	}
 	while (node != NULL)
 	{
 		toString(node->info);
-		node = node->next;
+		node = node->prev;
 	}
 
 	/* parse input arguments (policy, quantum (if required), input filename */
